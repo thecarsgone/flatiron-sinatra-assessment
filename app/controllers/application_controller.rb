36a6aa260a69c2f69bffd3285130b692dc2f7502
irevived1 <haifeng.wu@jjay.cuny.edu>
2016-07-25 23:12:52 -0400

    nothing works

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,21 +14,52 @@ class ApplicationController < Sinatra::Base
     set :session_secret, "password_security"
   end
 
+  #helper methods
+  helpers do
+    def logged_in?
+      !!session[:id]
+    end
+
+    def current_user
+      User.find(session[:id])
+    end
+  end
+
+  #main controller action
   get '/' do
+    erb :'login'
+  end
+
+  get '/index' do
     erb :index
   end
 
   get '/login' do
+    erb :login
   end
 
   get '/signup' do
     erb :'users/user_create'
   end
+
+  post '/' do
+    @user = User.find_by(username:params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:id] = @user.id
+      flash[:messsae] = "Successfully logged in."
+      redirect '/index'
+    else
+      flash[:messsae] = "Log in failed."
+      redirect '/login'
+    end
+  end
   
   post '/signup' do
-    User.create(username:params[:username],password:params[:password])
-    flash[:message] = "Successfully created user."
-    redirect '/login' do
+    redirect '/index' if logged_in?
+    if !params[:password].empty? && params[:username].empty?
+      User.create(username:params[:username],password:params[:password]).save
+      flash[:message] = "Successfully created user."
+      redirect '/login'
     end
   end
 

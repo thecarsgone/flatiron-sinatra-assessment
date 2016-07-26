756bd0a629d4636add8a667ca7e8ac4765837c7b
irevived1 <haifeng.wu@jjay.cuny.edu>
2016-07-25 22:49:28 -0400

    flash added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,10 @@
+require 'rack-flash'
 require './config/environment'
 
 class ApplicationController < Sinatra::Base
 #add this line for edit or delete in views
 #<input id="hidden" type="hidden" name="_method" value="patch/delete">
+  use Rack::Flash
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
@@ -10,6 +12,24 @@ class ApplicationController < Sinatra::Base
 		enable :sessions
 		#set :session_secret, "secret"
     set :session_secret, "password_security"
+  end
+
+  get '/' do
+    erb :index
+  end
+
+  get '/login' do
+  end
+
+  get '/signup' do
+    erb :'users/user_create'
+  end
+  
+  post '/signup' do
+    User.create(username:params[:username],password:params[:password])
+    flash[:message] = "Successfully created user."
+    redirect '/login' do
+    end
   end
 
 end

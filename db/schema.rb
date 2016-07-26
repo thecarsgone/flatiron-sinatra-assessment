d91f62c6a373d5046f247187c5b2795cf56f66aa
irevived1 <haifeng.wu@jjay.cuny.edu>
2016-07-25 21:13:41 -0400

    migrated db

--- db/schema.rb
+++ db/schema.rb
@@ -11,3 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+ActiveRecord::Schema.define(version: 20160726004953) do
+
+  create_table "pictures", force: :cascade do |t|
+    t.string  "name"
+    t.integer "user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
+  end
+
+end
